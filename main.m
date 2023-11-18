61bffc169f2bedca6e1fbccc208518a5f21e7369
PortalMaster555 <68411025+PortalMaster555@users.noreply.github.com>
2023-11-18 14:45:32 -0600

    added a helper function for the positive FFT

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ clc; printSplash(); clear all;
 audioFile = "test_440_48khz.wav";
 
 [Amps, Fs] = audioread(audioFile);
-N = size(Amps,1); % samples
+N = size(Amps,1); % number of samples
 fprintf("Audio file loaded: %s (length %d samples)\n", audioFile, N);
 
 fprintf("Playing sample of audio if available.\n")
@@ -11,21 +11,17 @@ audio = audioplayer(Amps,Fs); play(audio); pause(1.5); stop(audio);
 
 fprintf("Maximum detectable frequency at or just below  %f Hz\n", Fs/2);
 
+% Plot scaled input audio file vs. time
+scaleConst = Fs/25;
 figure(1);
 t = linspace(0,N/Fs,N);
-stem(Fs*t/25, Amps, ".k");
+fprintf("Length of audio file is %f seconds\n", max(t));
+stem(t*scaleConst, Amps, ".k");
 xlabel("Time (not to scale)"); ylabel("Amplitude");
 axis([0, max(t), -1, 1]);
 
-y = fft(Amps);
-absY = abs(y/N); %scaled two-sided amplitudes in f-space
-if mod(N,2) == 0
-    fourier = absY(1:N/2+1); % take the second half
-else
-    fourier = absY(1:(N+1)/2); % take the second half
-end
-fourier(2:end-1) = 2*fourier(2:end-1); % double all but 0 and nyq.
-
+% Compute full FFT for the input audio.
+fourier = pos_fft(Amps);
 f = linspace(0,Fs/2,size(fourier,1));
 figure(2); clf(2); plot(f,fourier,".b");
 
