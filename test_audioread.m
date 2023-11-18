27e5d3d5dd572dbc41c70ae13757d9d26893f0ca
PortalMaster555 <68411025+PortalMaster555@users.noreply.github.com>
2023-11-17 20:44:30 -0600

    tested on pure tone @440Hz

--- test_audioread.m
+++ test_audioread.m
@@ -1,9 +1,5 @@
 clc; clear all;
 
 % from MATLAB example code
-load handel.mat
-filename = 'test_handel.wav';
-audiowrite(filename,y,Fs);
-clear y Fs
-[y, Fs] = audioread("test_handel.wav");
+[y, Fs] = audioread("test_440_pure.mp3");
 sound(y,Fs);
