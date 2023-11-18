d30563a59339890cb493a497ef5e73aab49fa927
PortalMaster555 <68411025+PortalMaster555@users.noreply.github.com>
2023-11-17 20:42:22 -0600

    tested audioread on wav

--- test_audioread.m
+++ test_audioread.m
@@ -1,1 +1,9 @@
 clc; clear all;
+
+% from MATLAB example code
+load handel.mat
+filename = 'test_handel.wav';
+audiowrite(filename,y,Fs);
+clear y Fs
+[y, Fs] = audioread("test_handel.wav");
+sound(y,Fs);
