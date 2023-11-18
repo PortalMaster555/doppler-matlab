f2327d5cebdd602d67559a35bea561017ae873b7
PortalMaster555 <68411025+PortalMaster555@users.noreply.github.com>
2023-11-17 21:40:02 -0600

    Changed to 48kHz wav because mp3 kept inserting extra samples

--- main.m
+++ main.m
@@ -1,1 +1,16 @@
 clc; printSplash(); clear all;
+
+audioFile = "test_440_48khz.wav";
+
+[Amps, Fs] = audioread(audioFile);
+T = size(Amps,1) / Fs; % seconds
+fprintf("Audio file loaded: %s (length %f s)\n", audioFile, T);
+
+fprintf("Playing sample of audio if available.\n")
+audio = audioplayer(Amps,Fs);
+play(audio); pause(1); stop(audio);
+
+fprintf("Maximum detectable frequency at or just below  %f Hz\n", Fs/2);
+
+figure(1);
+
