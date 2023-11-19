148794c61f24a3fede8eb921fa11ade816c53625
PortalMaster555 <68411025+PortalMaster555@users.noreply.github.com>
2023-11-19 15:07:16 -0600

    Replaced the FFT with the STFT

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-clc; printSplash(); clear all;
+clc; printSplash(); figure(1); clf(1); clear all;
 
 audioFile = "50mphobserver.wav";
 
@@ -13,19 +13,29 @@ fprintf("Playback complete.\n")
 
 fprintf("Maximum detectable frequency at or just below  %f Hz\n", Fs/2);
 
-% Plot scaled input audio file vs. time
-figure(1);
-t = linspace(0,N/Fs,N);
-fprintf("Length of audio file is %f seconds\n", max(t));
-stem(t, Amps, ".k");
-xlabel("Time (s)"); ylabel("Amplitude");
-axis([0, max(t), -1, 1]);
+% Compute the short-time Fourier transform
+% Windowing function: length 2048 Hann
+% One-sided frequency range (absolute amplitude not preserved)
+WINDOW = hann(2048);
+[stfourier, f, t] = stft(Amps, Fs, FrequencyRange="onesided", ...
+    Window=WINDOW);
+ampStft = abs(stfourier);
 
-% Compute and plot full FFT for the input audio
-fourier = pos_fft(Amps);
-f = linspace(0,Fs/2,size(fourier,1));
-figure(2); clf(2); stem(f,fourier,".b");
-xlabel("Frequency (Hz)"); ylabel("Amplitude");
+deltaT = t(end)-t(end-1);
+deltaF = f(end)-f(end-1);
+%f,t,ampStft
 
+imagesc(t, f, ampStft);
+hold on;
+set(gca,'YDir','normal');
+xlabel("Time (s)");
+ylabel("Frequency (Hz)");
 
+changeIndices = findchangepts(ampStft,MaxNumChanges=2,Statistic="rms");
+beginT = changeIndices(1)*deltaT;
+endT = changeIndices(2)*deltaT;
 
+%plot vertical lines at largest changes
+xline(beginT, "r", LineWidth=1);
+xline(endT, "r", LineWidth=1);
+axis([0, max(t), 0, floor(max(f)*0.15)])
