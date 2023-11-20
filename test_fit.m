1b246c2b05ca06e5d958dfe9468678fa50cb60a8
PortalMaster555 <68411025+PortalMaster555@users.noreply.github.com>
2023-11-19 22:34:59 -0600

    isolated amplitudes outside the middle range

--- test_fit.m
+++ test_fit.m
@@ -1,6 +1,7 @@
 clc; clear all;  figure(2); clf(2); figure(1); clf(1);
 load("testvars50.mat");
 
+% Draw a colormap of the unsmoothed audio spectrogram
 c = "default"; colormap(c);
 imagesc(t, f, ampStft);
 hold on;
@@ -16,7 +17,7 @@ smoothAmps = smoothAmps';
 
 figure(2);
 c = "default"; colormap(c);
-imagesc(t, f, BW);
+%imagesc(t, f, smoothAmps);
 hold on;
 set(gca,'YDir','normal');
 xlabel("Time (s)");
@@ -28,5 +29,31 @@ xline(endT, "r", LineWidth=1);
 
 % Calculate sum of each frequency band across time
 % outside of the middle region
+begRange = 1:changeIndices(1);
+endRange = changeIndices(2):size(smoothAmps,2);
+begAmps = smoothAmps(:, begRange);
+endAmps = smoothAmps(:, endRange);
+tbeg = t(begRange); tend = t(endRange);
+imagesc(tbeg, f, begAmps);
+imagesc(tend, f, endAmps);
 
-beginningAmps = smoothAmps(:,)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
