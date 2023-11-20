cdb9c609a2d6f32a2ce389ba32f1a496f840423f
PortalMaster555 <68411025+PortalMaster555@users.noreply.github.com>
2023-11-19 23:26:24 -0600

    technically successful speed analysis from audio (46.191431 mph vs. 50 mph)

--- test_velocity_analysis.m
+++ test_velocity_analysis.m
@@ -1,5 +1,5 @@
 clc; clear all;
-
+load("test_highestf.mat");
 % Generate sample signal
 
 
@@ -14,15 +14,31 @@ app = fObserver(-trueV, 0, c, fSource);
 rec = fObserver(trueV, 0, c, fSource);
 % SUBSTITUTE MEASURED app, rec VALUES HERE
 
+
 % only works for stationary observer in line with the vehicle
 % technically there should be a "radial" term for v
-velFcn = @(v) (c-v)*app - (c+v)*rec;
-sourceV = fsolve(velFcn, 0);
 
-fprintf("Approach: %f Hz\nRecede: %f Hz\nTrue: %f Hz\n", ...
-    app, rec, fSource);
-fprintf("True velocity: %f m/s\nEstimated velocity: %f m/s\n", ...
-    trueV, sourceV);
+options = optimset('Display','off');
+for i = 1:size(highestFBeg, 1)
+    app(i) = highestFBeg(i);
+    rec(i) = highestFEnd(i);
+    velFcn = @(v) (c-v)*app(i) - (c+v)*rec(i);
+    sourceV(i) = fsolve(velFcn, 0, options);
+    fprintf("Approach: %f Hz\nRecede: %f Hz\nVel Est.: %f m/s\n\n", ...
+    app(i), rec(i), sourceV(i));
+end
+
+%finds the index with the minimal difference in A
+minDiffInd = find(abs(diff(sourceV))==min(abs(diff(sourceV))));
+%extract this index, and it's neighbor index from A
+v1 = sourceV(minDiffInd);
+v2 = sourceV(minDiffInd+1);
+
+avgClosestV = mean([v1, v2]);
+% Convert to MPH from m/s
+avgClosestV = 2.237 * avgClosestV;
+
+fprintf("Estimated velocity: %f mph\n", avgClosestV);
 
 %vSList();
 
